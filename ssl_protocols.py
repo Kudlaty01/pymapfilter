e43de956ae5b95bea1b7d5c0b88d093955a41135
Paweł Sola <kudlok@mail.ru>
2019-01-29 10:21:26 +0100

    Added importable ssl_protocols dict with ssl predefined protocols (for simplification in configs)

---
+++ ssl_protocols.py
@@ -0,0 +1,11 @@
+import ssl;
+ssl_protocols = {
+    'ssl': ssl.PROTOCOL_SSLv23,
+    'ssl23': ssl.PROTOCOL_SSLv23,
+    'tls': ssl.PROTOCOL_TLS,
+    'tls1': ssl.PROTOCOL_TLSv1,
+    'tls11': ssl.PROTOCOL_TLSv1_1,
+    'tls1.1': ssl.PROTOCOL_TLSv1_1,
+    'tls12': ssl.PROTOCOL_TLSv1_2,
+    'tls1.2': ssl.PROTOCOL_TLSv1_2
+}
