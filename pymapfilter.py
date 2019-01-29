522f7f9787ac8688b779ebfea45d5f5a6cf8ad39
Paweł Sola <kudlok@mail.ru>
2019-01-29 15:33:41 +0100

    Main file code reorganization it lazy-initialized imap_server

--- pymapfilter.py
+++ pymapfilter.py
@@ -9,41 +9,52 @@ class PyMapFilter(object):
     """class for mail filtering based on accounts"""
     def __init__(self, account):
         """protocols initialization"""
+        self.account = account
         pass
+
+    def _get_imap_server(self):
+        """docstring for _get_imap_server"""
+        try:
+            self.imap_server.check()
+        except Exception as e:
+            account = self.account
+            sslC = SSLContext(ssl_protocols[account.ssl])
+            self.imap_server = IMAP4_SSL(account.server, account.port, ssl_context = sslC)
+            self.imap_server.login(account.login, account.password)
+        return self.imap_server
+
 
     def filterAccount(self):
         """filter configured account by it's redirections"""
-        sslC = SSLContext(ssl_protocols[account.ssl])
-
-        self.imap_server = IMAP4_SSL(account.server, account.port, ssl_context = sslC)
-        self.imap_server.login(account.login, account.password)
+        account = self.account
+        imap_server = self._get_imap_server()
         try:
-            self.imap_server.select()
+            imap_server.select()
             for target in account.redirections:
                 print('moving to:\t%s' % target)
                 for redir in account.redirections[target]:
                     print('processing query:\t%s' % redir)
-                    typ, data = self.imap_server.search(None, redir)
+                    typ, data = imap_server.search(None, redir)
                     messages = data[0].split()
                     for msg in messages:
-                        self.imap_server.copy(msg, target)
-                        self.imap_server.store(msg, '+FLAGS', '\\Deleted')
+                        imap_server.copy(msg, target)
+                        imap_server.store(msg, '+FLAGS', '\\Deleted')
                         sys.stdout.write('.')
                     print('moved %d messages' % len(messages))
         except Exception as e:
             print('An error occured!')
             # raise e
-        self.imap_server.close()
-        self.imap_server.logout()
+        imap_server.close()
+        imap_server.logout()
 
 def main(argv):
     """main function to be executed id file run from command line"""
     print("let's process some messages!")
-    pymap_filter = PyMapFilter()
     for account in accounts:
+        pymap_filter = PyMapFilter(account)
         if account.name != None:
             print("Now processing: %s" % account.name)
-        pymap_filter.filterAccount(account)
+        pymap_filter.filterAccount()
     print("all done!")
 
 
