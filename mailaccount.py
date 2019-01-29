0a968a8b6eda1de61dfe88df426a127358af44de
Paweł Sola <kudlok@mail.ru>
2019-01-29 10:20:33 +0100

    Added MailAccount data holding class

---
+++ mailaccount.py
@@ -0,0 +1,17 @@
+class MailAccount(object):
+    """Mail Account data holder class"""
+    def __init__(self, server, login, password, port = 993, ssl = 'tls1', redirections = None, name = None):
+        """initialization of the MailAccount class
+        server: the imap mail server
+        login: login form email
+        password: mail account password
+        port: optional port (default 993)
+        ssl: one of the SSL dict keys (default 'tls1')
+        redirections: (a dict) where to move selected mail in format Target -> [array of conditions]"""
+        self.server = server
+        self.login = login
+        self.password = password
+        self.port = port
+        self.ssl = ssl
+        self.redirections = redirections
+        self.name = name
